18b14693fb782b71a60392feea4204c870c1fce9
Donald Kirk <dkirk9325@gmail.com>
2020-09-08 21:15:10 -0500

    updated gcloud stuff

--- src/main/java/com/clincaloutcome/EUClinicalTrialApplication.java
+++ src/main/java/com/clincaloutcome/EUClinicalTrialApplication.java
@@ -4,6 +4,7 @@ import com.clincaloutcome.filestorage.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @EnableConfigurationProperties({FileStorageProperties.class})
